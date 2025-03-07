0b764d77db0c95a2b47cf09782983a4250638d01
Zach Morek <zmorek@gmail.com>
2025-03-07 10:54:24 -0500

    viewport

--- lib/doom/viewport.rb
+++ lib/doom/viewport.rb
@@ -1,40 +1,25 @@
-module Doom
-  class Viewport
-    attr_accessor :scale
-    attr_reader :width, :height, :scale
+class Viewport
+  BASE_WIDTH = 320
+  BASE_HEIGHT = 200
 
-    def initialize
-      @width = 320
-      @height = 200
-      @scale = 1
-    end
+  attr_accessor :scale
+  attr_reader :width, :height
 
-    def scale=(value)
-      @scale = value.to_f
-    end
+  def initialize
+    @width = BASE_WIDTH
+    @height = BASE_HEIGHT
+    @scale = 1
+  end
 
-    def aspect_ratio
-      @width.to_f / @height
-    end
+  def aspect_ratio
+    width.to_f / height
+  end
 
-    def scaled_width
-      (@width * @scale).to_i
-    end
+  def scaled_width
+    width * scale
+  end
 
-    def scaled_height
-      (@height * @scale).to_i
-    end
-
-    def centerx
-      scaled_width / 2
-    end
-
-    def centery
-      scaled_height / 2
-    end
-
-    def scaled_aspect_ratio
-      scaled_width.to_f / scaled_height
-    end
+  def scaled_height
+    height * scale
   end
 end
--- test/doom/viewport_test.rb
+++ test/doom/viewport_test.rb
@@ -1,84 +1,24 @@
-require 'test_helper'
-require 'doom/viewport'
+require 'minitest/autorun'
+require_relative '../../lib/doom/viewport'
 
-module Doom
-  class ViewportTest < Minitest::Test
-    def setup
-      @viewport = Viewport.new
-    end
+class ViewportTest < Minitest::Test
+  def setup
+    @viewport = Viewport.new
+  end
 
-    def test_maintains_320x200_resolution
-      assert_equal 320, @viewport.width
-      assert_equal 200, @viewport.height
-    end
+  def test_maintains_base_resolution
+    assert_equal 320, @viewport.width
+    assert_equal 200, @viewport.height
+  end
 
-    def test_handles_aspect_ratio_correction
-      # 320x200 has a 16:10 aspect ratio
-      assert_in_delta(1.6, @viewport.aspect_ratio)
-    end
+  def test_aspect_ratio
+    assert_in_delta(1.6, @viewport.aspect_ratio)
+  end
 
-    def test_supports_integer_scaling
-      @viewport.scale = 2
+  def test_integer_scaling
+    @viewport.scale = 2
 
-      assert_equal 640, @viewport.scaled_width
-      assert_equal 400, @viewport.scaled_height
-    end
-
-    def test_calculates_centerx_and_centery
-      assert_equal 160, @viewport.centerx
-      assert_equal 100, @viewport.centery
-    end
-
-    def test_handles_view_size_changes
-      @viewport.scale = 3
-
-      assert_equal 960, @viewport.scaled_width
-      assert_equal 600, @viewport.scaled_height
-      assert_equal 480, @viewport.centerx
-      assert_equal 300, @viewport.centery
-    end
-
-    def test_handles_zero_scale
-      @viewport.scale = 0
-
-      assert_equal 0, @viewport.scaled_width
-      assert_equal 0, @viewport.scaled_height
-      assert_equal 0, @viewport.centerx
-      assert_equal 0, @viewport.centery
-    end
-
-    def test_handles_negative_scale
-      @viewport.scale = -1
-
-      assert_equal(-320, @viewport.scaled_width)
-      assert_equal(-200, @viewport.scaled_height)
-      assert_equal(-160, @viewport.centerx)
-      assert_equal(-100, @viewport.centery)
-    end
-
-    def test_handles_decimal_scale
-      @viewport.scale = 1.5
-
-      assert_equal 480, @viewport.scaled_width
-      assert_equal 300, @viewport.scaled_height
-      assert_equal 240, @viewport.centerx
-      assert_equal 150, @viewport.centery
-    end
-
-    def test_maintains_aspect_ratio_at_all_scales
-      scales = [1, 2, 3, 4, 5, 1.5, 2.5, 3.5]
-      scales.each do |scale|
-        @viewport.scale = scale
-
-        assert_in_delta(1.6, @viewport.scaled_width.to_f / @viewport.scaled_height)
-      end
-    end
-
-    def test_center_points_are_integers
-      @viewport.scale = 1.5
-
-      assert_kind_of(Integer, @viewport.centerx)
-      assert_kind_of(Integer, @viewport.centery)
-    end
+    assert_equal 640, @viewport.scaled_width
+    assert_equal 400, @viewport.scaled_height
   end
 end
